6a99286eee5dd37a8a63c5206ea367afe8c04b80
mohammad forhad hossain <forhad83@gmail.com>
2017-02-13 10:20:46 +0100

    no message

---
+++ src/models/NewStudent.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+
+/**
+ *
+ * @author jarltuxen
+ */
+public class Student {
+    private String rollNo;
+    private String name;
+    private int age;
+    private double age;
+   
+    public String getRollNo() {
+        return rollNo;
+    }
+   
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
